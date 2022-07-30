ef8856ad6e4647781f692e356e0f81ccc5fc1e07
Cristian Ruiz <cristianrb235@gmail.com>
2022-07-30 11:19:36 +0200

    Add day 8

---
+++ 2015/Day-08/src/cmd/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"cristianrbaoc2015d8/src/io"
+	"strconv"
+)
+
+func main() {
+	lines, _ := io.ReadLines("inputs/input_01")
+	total1 := countTotalCharactersDecoded(lines)
+	println(total1)
+	total2 := countTotalCharactersEncoded(lines)
+	println(total2)
+
+}
+
+func countTotalCharactersDecoded(lines []string) int {
+	total := 0
+	for _, line := range lines {
+		total += len(line) - len(unquote(line))
+	}
+	return total
+}
+
+func countTotalCharactersEncoded(lines []string) int {
+	total := 0
+	for _, line := range lines {
+		total += len(quote(line)) - len(line)
+	}
+	return total
+}
+
+func unquote(str string) string {
+	s, _ := strconv.Unquote(str)
+	return s
+}
+
+func quote(str string) string {
+	return strconv.Quote(str)
+}
